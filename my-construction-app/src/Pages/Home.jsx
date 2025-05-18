d149ed2f39da82970ce0cb491049ece963598d9c
Md Gulfam <mdgulfam0505@gmail.com>
2025-05-18 23:35:05 +0530

    chooseUs

--- my-construction-app/src/Pages/Home.jsx
+++ my-construction-app/src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import Services from '../Components/Services'
-import WhyChooseUs from '../components/WhyChooseUs';
+import WhyChooseUs from '../Components/whyChooseUs';
 import Testimonials from '../Components/Testimonials';
 // import Hero from '../Components/Hero'
 
