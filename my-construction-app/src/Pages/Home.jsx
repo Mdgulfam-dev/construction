ba3dade3dcac0bcfef4f3814108c5ccdb8991d7a
Md Gulfam <mdgulfam0505@gmail.com>
2025-05-18 23:39:43 +0530

    Fix casing for WhyChooseUs import

--- my-construction-app/src/Pages/Home.jsx
+++ my-construction-app/src/Pages/Home.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import Hero from '../Components/Hero'
-import Services from '../Components/Services'
-import WhyChooseUs from '../Components/whyChooseUs';
+import React from 'react';
+import Hero from '../Components/Hero';
+import Services from '../Components/Services';
+import WhyChooseUs from '../Components/WhyChooseUs';
 import Testimonials from '../Components/Testimonials';
-// import Hero from '../Components/Hero'
-
 
 const Home = () => {
   return (
     <div>
-     
-     <Hero/>
-     <Services/>
-     <WhyChooseUs/>
-     <Testimonials/>
+      <Hero />
+      <Services />
+      <WhyChooseUs />
+      <Testimonials />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
