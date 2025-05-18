9523529bf70cfc68ae32874597b2e8e33484d918
Md Gulfam <mdgulfam0505@gmail.com>
2025-05-18 23:48:32 +0530

    frontednurl

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Enhanced CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://construction-44rn.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
