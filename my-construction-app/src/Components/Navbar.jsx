0ce95a5467aa6a435d00d13ebd3771393f2d35bb
Md Gulfam <mdgulfam0505@gmail.com>
2025-07-25 21:58:36 +0530

    name-update

--- my-construction-app/src/Components/Navbar.jsx
+++ my-construction-app/src/Components/Navbar.jsx
@@ -232,8 +232,8 @@ const Navbar = () => {
             to="/home"
             className="text-xl sm:text-2xl font-bold text-blue-600 tracking-wide"
           >
-            Renova
-            <span className="text-gray-800">x</span>
+            Renovax
+            
           </Link>
 
           {/* Desktop Menu */}
